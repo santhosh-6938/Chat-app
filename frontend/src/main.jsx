46f15fe495fe1b8b473c5b004289c7dd1ab49d53
santhosh-6938 <santhoshbikkina369@gmail.com>
2025-07-05 01:02:14 +0530

    project completed

---
+++ frontend/src/main.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client'; // or from 'react-dom' if using older React
+
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+
+import { BrowserRouter } from "react-router-dom";
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>
+);
